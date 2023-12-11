f5172b82a0579143f02f8a347af3f7fce27dbcf1
DEV-COM <kwp@tlen.pl>
2023-12-11 15:00:04 +0100

    15:00 11.12

--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify, Blueprint
 
 from my_app import db
-from my_app.catalog.models import Product
+from my_app.catalog.models import Product, Category
 
 catalog = Blueprint('catalog', __name__)
 
@@ -25,8 +25,26 @@ def products():
     for product in products:
         res[product.id] = {
             'name': product.name,
-            'price': str(product.price)
+            'price': str(product.price),
+            'category': product.category.name
         }
+    return jsonify(res)
+
+
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    res = {}
+    for category in categories:
+        res[category.id] = {
+            'name': category.name
+        }
+        for product in category.products:
+            res[category.id]['products'] = {
+                'id': product.id,
+                'name': product.name,
+                'price': str(product.price)
+            }
     return jsonify(res)
 
 
@@ -34,7 +52,19 @@ def products():
 def create_product():
     name = request.form.get('name')
     price = request.form.get('price')
-    product = Product(name, price)
+    categ_name = request.form.get('category')
+    category = Category.query.filter_by(name=categ_name).first()
+    if not category:
+        category = Category(categ_name)
+    product = Product(name, price, category)
     db.session.add(product)
     db.session.commit()
     return "Stworzono produkt"
+
+@catalog.route('/category-create', methods=['POST', ])
+def create_category():
+    name = request.form.get('name')
+    category = Category(name)
+    db.session.add(category)
+    db.session.commit()
+    return "Stworzono kategorię"
