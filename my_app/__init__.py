27f7f37cedc898a3187f76ecfb8cb2819c71e8ed
DEV-COM <kwp@tlen.pl>
2023-12-11 14:22:55 +0100

    14:22 11.12

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
 
 from my_app.hello.views import my_blueprint
 from my_app.product.views import product_blueprint
@@ -8,13 +8,18 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 # app.config["SQLALCHEMY_DATABASE_URI"] = f"mariadb://{1254}:{12345}@localhost/exampledbflask"
-app.register_blueprint(my_blueprint)
-app.register_blueprint(product_blueprint)
+# app.register_blueprint(my_blueprint)
+# app.register_blueprint(product_blueprint)
 
-class Base(DeclarativeBase):
-  pass
+# class Base(DeclarativeBase):
+#   pass
 
-db = SQLAlchemy(app, model_class=Base)
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
+
+from my_app.catalog.views import catalog
+app.register_blueprint(catalog)
 
 with app.app_context():
     db.create_all()
---
+++ my_app/catalog/views.py
@@ -0,0 +1,38 @@
+from flask import request, jsonify, Blueprint
+
+from my_app import db
+from my_app.catalog.models import Product
+
+catalog = Blueprint('catalog', __name__)
+
+
+@catalog.route('/')
+@catalog.route('/home')
+def home():
+    return 'Witamy na stronie katalogu!'
+
+
+@catalog.route('/product/<id>')
+def product(id):
+    product = Product.query.get_or_404(id)
+    return 'Produkt - %s, PLN %s' % (product.name, product.price)
+
+@catalog.route('/products')
+def products():
+    products = Product.query.all()
+    res = {}
+    for product in products:
+        res[product.id] = {
+            'name': product.name,
+            'price': str(product.price)
+        }
+    return jsonify(res)
+
+@catalog.route('/product-create', methods=['POST',])
+def create_product():
+    name = request.form.get('name')
+    price = request.form.get('price')
+    product = Product(name,price)
+    db.session.add(product)
+    db.session.commit()
+    return "Stworzono produkt"
