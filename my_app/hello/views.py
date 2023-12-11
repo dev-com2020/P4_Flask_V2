4e322e67323b9a0fbea022abccca47647e353048
DEV-COM <kwp@tlen.pl>
2023-12-11 10:44:24 +0100

    10:44 11.12

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -3,14 +3,23 @@ from my_app.hello.models import MESSAGES
 
 my_blueprint = Blueprint('my_blueprint', __name__)
 
+
 @my_blueprint.route('/')
 def hello():
     return MESSAGES['default']
 
-@my_blueprint.route('/show')
+
+@my_blueprint.route('/show/')
 def all_keys():
-    return MESSAGES['default']
+    return f"{list(MESSAGES.keys())}"
+
 
 @my_blueprint.route('/show/<key>')
 def get_message(key):
     return MESSAGES.get(key) or "%s nie znaleziony" % key
+
+
+@my_blueprint.route('/show/<key>/<message>')
+def add_or_update_message(key, message):
+    MESSAGES[key] = message
+    return f" {key} dodano/zaktualizowano"
