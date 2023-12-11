93a23b4b714966453c303b4f5fa32889e2fb6d34
DEV-COM <kwp@tlen.pl>
2023-12-11 14:35:47 +0100

    14:35 11.12

--- my_app/catalog/models.py
+++ my_app/catalog/models.py
@@ -11,4 +11,15 @@ class Product(db.Model):
         self.price = price
 
     def __repr__(self):
-        return f'Product: {id}'
+        return f'Product: {self.id}'
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Kategoria: {self.id}'
