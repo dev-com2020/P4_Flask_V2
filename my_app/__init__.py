28d2ddb55191669f8ba58a724404e8c00b2fe37d
DEV-COM <kwp@tlen.pl>
2023-12-11 13:54:09 +0100

    13:54 11.12

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from sqlalchemy.orm import DeclarativeBase
 
 from my_app.hello.views import my_blueprint
 from my_app.product.views import product_blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+# app.config["SQLALCHEMY_DATABASE_URI"] = f"mariadb://{1254}:{12345}@localhost/exampledbflask"
 app.register_blueprint(my_blueprint)
 app.register_blueprint(product_blueprint)
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+with app.app_context():
+    db.create_all()
