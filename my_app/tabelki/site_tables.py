5a8528e3ad81c6dfe8c30cc09548ba1b5c84ef7d
DEV-COM <kwp@tlen.pl>
2023-12-12 14:58:01 +0100

    14:57 12.12

--- my_app/tabelki/site_tables.py
+++ my_app/tabelki/site_tables.py
@@ -4,14 +4,14 @@ import pandas as pd
 app4 = Flask(__name__)
 
 
-@app4.route('/tables')
+@app4.route('/')
 def show_tables():
     data = pd.read_excel('imiona.xlsx', sheet_name="Wynik2")
     data.set_index(['Name'], inplace=True)
     data.index.name = None
     females = data.loc[data.Gender == 'k']
     males = data.loc[data.Gender == 'm']
-    return render_template('view.html', tables=[females.to_html(classes='female'),
+    return render_template('view2.html', tables=[females.to_html(classes='female'),
                                                 males.to_html(classes='male')],
                            titles=['na', 'Female students', 'Male students'])
 
